f6052c7ac9770ac92e89cb3ea9e8a143e6533c0e
jerinzech <jerinzjames@gmail.com>
2023-07-19 13:55:47 +0530

    prefers color scheme is broken now

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,41 @@
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
+
+const setColorMode = () => {
+  if (localStorage.getItem("colorMode") == "dark") {
+    setDarkMode();
+    darkButton.click();
+  } else if (localStorage.getItem("colorMode") == "light") {
+    setLightMode();
+    lightButton.click();
+  }
+};
+
+const checkIsLight = () => {
+  if (window.matchMedia("(prefers-color-scheme:light)").matches) {
+    lightButton.click();
+  }
+};
+
+const setDarkMode = () => {
+  document.querySelector("body").classList = "dark";
+};
+
+const setLightMode = () => {
+  document.querySelector("body").classList = "light";
+};
+
+setColorMode();
+
 const radioButtons = document.querySelectorAll(".toggle__wrapper input");
 for (let i = 0; i < radioButtons.length; i++) {
   radioButtons[i].addEventListener("click", (event) => {
-    document.getElementById("dark").checked
-      ? (document.querySelector("body").classList = "dark")
-      : (document.querySelector("body").classList = "light");
+    if (darkButton.checked) {
+      localStorage.setItem("colorMode", "dark");
+      setDarkMode();
+    } else {
+      localStorage.setItem("colorMode", "light");
+      setLightMode();
+    }
   });
 }
